64a32f0bbf7d29e4802607dffd42990b807be070
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-10-07 23:20:18 +0300

    Initial Commit

---
+++ InteractiveWidgetKit/InteractiveWidgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveWidgetKitApp.swift
+//  InteractiveWidgetKit
+//
+//  Created by Beyza Nur Tekerek on 7.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveWidgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
